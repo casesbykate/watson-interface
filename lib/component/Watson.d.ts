8b6ee5e6ad87febaa75331913a831fabf7a63737
Hanul <hanul@hanul.me>
2021-12-18 20:59:21 +0900

    add xtern

---
+++ lib/component/Watson.d.ts
@@ -0,0 +1,5 @@
+import { DomNode } from "@hanul/skynode";
+export default class Watson extends DomNode {
+    constructor();
+}
+//# sourceMappingURL=Watson.d.ts.map
---
+++ lib/view/Command.d.ts
@@ -0,0 +1,9 @@
+import { View, ViewParams } from "skyrouter";
+export default class Command implements View {
+    private container;
+    private interval;
+    constructor();
+    changeParams(params: ViewParams, uri: string): void;
+    close(): void;
+}
+//# sourceMappingURL=Command.d.ts.map
